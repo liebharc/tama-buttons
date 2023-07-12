89af7ffe8a15b35847c5b77bde43585933127e71
Christian Liebhardt <christian.liebhardt@arcor.de>
2023-07-12 17:02:42 +0200

    Added button examples based on https://tamagui.dev/docs/components/button/1.28.0

--- packages/app/features/home/screen.tsx
+++ packages/app/features/home/screen.tsx
@@ -13,6 +13,52 @@ import { ChevronDown, ChevronUp } from '@tamagui/lucide-icons'
 import React, { useState } from 'react'
 import { useLink } from 'solito/link'
 
+import { Activity, Airplay } from '@tamagui/lucide-icons'
+import { XGroup } from 'tamagui'
+
+const repeatFactor = 50;
+
+export function ButtonDemo(props) {
+  return (
+    <YStack padding="$3" space="$15" {...props}>
+      <Button backgroundColor="red">{"Plain ".repeat(repeatFactor)}</Button>
+      <Button backgroundColor="red" alignSelf="center" icon={Airplay} size="$6">
+        {"Large ".repeat(repeatFactor)}
+      </Button>
+      <XStack space="$2" justifyContent="center">
+        <Button size="$3" theme="alt2">
+          {"Alt2 ".repeat(repeatFactor)}
+        </Button>
+        <Button size="$3" theme="yellow">
+          {"Yellow ".repeat(repeatFactor)}
+        </Button>
+      </XStack>
+      <XStack space="$2">
+        <Button themeInverse size="$3">
+          {"Inverse ".repeat(repeatFactor)}
+        </Button>
+        <Button iconAfter={Activity} size="$3">
+          {"iconAfter ".repeat(repeatFactor)}
+        </Button>
+      </XStack>
+      <XGroup>
+        <XGroup.Item>
+          <Button width="50%" size="$2" disabled opacity={0.5}>
+            {"disabled ".repeat(repeatFactor)}
+          </Button>
+        </XGroup.Item>
+
+        <XGroup.Item>
+          <Button width="50%" size="$2" chromeless>
+            {"chromeless ".repeat(repeatFactor)}
+          </Button>
+        </XGroup.Item>
+      </XGroup>
+    </YStack>
+  )
+}
+
+
 export function HomeScreen() {
   const linkProps = useLink({
     href: '/user/nate',
@@ -48,6 +94,8 @@ export function HomeScreen() {
       <XStack>
         <Button {...linkProps}>Link to user</Button>
       </XStack>
+
+      <ButtonDemo />
 
       <SheetDemo />
     </YStack>
